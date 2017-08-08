953ea2e9815b1af46cf9b70b22392b70e7b829a5
Jing Jia <nycjing@gmail.com>
2017-08-08 12:50:27 -0400

    setTimeout

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,18 +1,21 @@
 var expect = require('chai').expect;
+
+function done(){
+    return console.log('Done' + err);
+};
+
 describe('+', function () {
     var posNum, negNum;
     beforeEach(function () {
         posNum = 2;
         negNum = -2;
     });
-    it('sums two positive numbers', function () {
+
+    it('sums two positive numbers,confirms basic arithmetic', function () {
         var result = posNum + posNum;
         expect(result).to.equal(4);
     });
-    // it('sums two negative number', function () {
-    //     var result = negNum + negNum;
-    //     expect(result).to.equal(-100);
-    // });
+
     // it('sums a negative and a positive', function () {
     //     var result = negNum + posNum;
     //     expect(result).to.equal(50);
@@ -22,4 +25,20 @@ describe('+', function () {
     //         resultB = posNum + negNum;
     //     expect(resultA).to.equal(resultB);
     // });
+});
+
+describe('setTimeout function', function () {
+
+
+    it('should show about 1 s', function (done) {
+        var current = new Date();
+        setTimeout(function () {
+            result = new Date() - current;
+            console.log(result);
+            expect(result).to.be.closeTo(1000, 50);
+            done();
+        }, 1000);
+
+
+    });
 });
