312824dbbb76bd27b80770180b92f7ddb8afe1b2
Jing Jia <nycjing@gmail.com>
2017-08-09 10:17:38 -0400

    findSimilar

--- models/index.js
+++ models/index.js
@@ -79,7 +79,7 @@ Page.prototype.findSimilar = function () {
   return Page.findAll({
     where: {
       tags: {
-        $overlap: this.tags
+        $contains: this.tags
       },
       id: {
         $ne: this.id
--- test/modeltest.js
+++ test/modeltest.js
@@ -1,5 +1,7 @@
 var models = require('../models');
-var Page = models.Page;
+var Page = require('../models').Page;
+var db = require('../models').db;
+var User = models.User;
 var expect = require('chai').expect;
 var chai = require('chai');
 var spies = require('chai-spies');
@@ -9,73 +11,119 @@ chai.use(spies);
 
 describe('Testing Page model', function (done) {
 
-    beforeEach(function(){
-        Page.sync({force:true})
-            .then(done)
-            .catch(done);
+    before('Sync and reset whole db', () => {
+        return db.sync({force: true})
     });
+
+    beforeEach('Sync and reset the pages table', () => {
+        return Page.sync({force: true})
+    })
 
     describe('First part: Virtuals', function () {
         var page;
-        beforeEach(function(){
-            page=Page.build({
-                title: 'our page',
-                urlTitle:'our_page',
-                content:'# life is fun',
-                status:"open",
-                tags:['life','fun']
-            })
+        beforeEach(function () {
+        page = Page.build({
+            title: 'our page',
+            urlTitle: 'our_page',
+            content: '# life is fun',
+            status: "open",
+            tags: ['life', 'fun']
         });
-        describe('Virtal 1: route', function () {
-            it('returns the url_name prepended by "/wiki/"', function(){
-                var result = page.route;
-                expect(result).to.equal("/wiki/our_page");
-            });
+    });
+    describe('Virtal 1: route', function () {
+        it('returns the url_name prepended by "/wiki/"', function () {
+            var result = page.route;
+            expect(result).to.equal("/wiki/our_page");
         });
-        describe('Virtal 2: renderedContent', function () {
-            it('converts the markdown-formatted content into HTML', function(){
-                var result = page.renderedContent;
-                expect(result).to.equal(marked('# life is fun'));
+    });
+    describe('Virtal 2: renderedContent', function () {
+        it('converts the markdown-formatted content into HTML', function () {
+            var result = page.renderedContent;
+            expect(result).to.equal(marked('# life is fun'));
 
-            });
         });
+    });
     });
 
     describe('Class methods', function () {
 
-        before(function (done) {
+        const pageData = [
+            {title: 'Sunrise', content: 'I never see this', tags: ['sun']},
+            {title: 'Daytime', content: 'Busy', tags: ['indoors']},
+            {title: 'Sunset', content: 'Beautiful', tags: ['sun']},
+        ];
 
-            Page.create({
-            title: 'foo',
-            content: 'bar',
-            tags: ['foo', 'bar']
+        beforeEach('put pages into db', () => {
+            return Promise.all(pageData.map(pageDatum => {
+              return Page.create(pageDatum)
+            }))
+            // return Page.bulkCreate(pageData) // I like this —GLL
         })
-        .then(function () {
-            done();
-        })
-        .catch(done);
-        });
 
         describe('findByTag', function () {
-            it('gets pages with the search tag', function (done) {
-            Page.findByTag('bar')
-            .then(function (pages) {
-                expect(pages).to.have.lengthOf(1);
-                done();
-            })
-            .catch(done);
+
+            it('gets pages with the search tag', function () {
+                return  Page.findByTag('sun')
+
+                    .then(function (page) {
+                        console.log('what is pages ID',page[0].id );
+                        expect(page).to.have.lengthOf(2);
+                    })
+            });
+            it('does not get pages without the search tag', function () {
+                return  Page.findByTag('bar')
+                    .then(function (page) {
+                        expect(page).to.have.lengthOf(0);
+                    })
             });
 
-            it('does not get pages without the search tag', function(){
+    });
 
-            });
-        });
     });
 
     describe('Instance methods', function () {
+
+
         describe('findSimilar', function () {
-            it('never gets itself');
-            it('gets other pages with any common tags');
+
+            const pageData = [
+                {title: 'Sunrise', content: 'I never see this', tags: ['goo']},
+                {title: 'Daytime', content: 'Busy', tags: ['indoors','goo']},
+                {title: 'Sunset', content: 'Beautiful', tags: ['sun','goo']},
+            ];
+            var page;
+            beforeEach('put pages into db', () => {
+                return Promise.all(pageData.map(pageDatum => {
+                    return Page.create(pageDatum)
+                }))
+        .then(pages=>{
+               return page = pages[0];
+
+
+        })
+
+        })
+
+            it('never gets itself', function () {
+
+                return page.findSimilar()
+                    .then(pages=>{
+                    console.log('what is pages ID',pages.id,'wahts is pages ID',page.id );
+                    expect(pages).to.not.have.property('id', page.id)
+
+            })
+            });
+
+            it('gets other pages with any common tags', function () {
+                return page.findSimilar()
+                    .then(pages=>{
+                    console.log('what is pages tags',pages.tags,'wahts is pages tags',page.tags );
+                    var result = pages.tags;
+                console.log('------',result);
+                expect(pages.tags).to.have.members(page.tags);
+            })
+            });
+
             it('does not get other pages without any common tags');
         });
     });
